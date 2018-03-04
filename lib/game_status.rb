a3403a7c12ee15e16711f68b9ff1d0e1ac607df5
Mark Cruz <markchriscruz@yahoo.com>
2018-03-04 21:06:34 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -71,13 +71,10 @@ end
 
 def winner(array)
   if won?(array)
-    #return true
     WIN_COMBINATIONS.each do |win_combination|
-      #puts win_combination
       position_1 = array[win_combination[0]]
       position_2 = array[win_combination[1]]
       position_3 = array[win_combination[2]]
-      #puts "p1" + position_1
       if (position_1 == "X" && position_2 == "X" && position_3 == "X")
         return "X"
       elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
